beadbc668ccc3a22eaf32cc800c7ffa7e529856e
lllinden <linden.wangxinyu@gmail.com>
2015-03-30 19:30:32 +0100

    wrong group commit, so I delete them

--- 0_replace_missing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('./SPEED/taxi_pm_std.csv')
-df1 = pd.read_csv('./SPEED/taxi_pm_avg.csv')
-df = df.fillna(method='pad')
-df1 = df.fillna(method='pad')
-df.to_csv('./SPEED/taxi_pm_std.csv')
-df1.to_csv('./SPEED/taxi_pm_avg.csv')
