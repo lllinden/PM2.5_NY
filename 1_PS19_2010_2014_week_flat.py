b37104f4ec1e703423b8379a087dc203901d4193
lllinden <linden.wangxinyu@gmail.com>
2015-04-22 09:22:57 -0400

    Polar plot for ps19 2010-2014

---
+++ 1_PS19_2010_2014_week_flat.py
@@ -0,0 +1,84 @@
+import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+from numpy import genfromtxt
+from scipy import signal
+
+def rearrange(average, year):
+	if year == 2010:
+		avg = np.hstack((average[72:],average[:72]))
+	if year == 2011:
+		avg = np.hstack((average[48:],average[:48]))
+	if year == 2012:
+		avg = np.hstack((average[24:],average[:24]))
+		# print average
+	if year == 2013:
+		avg = np.hstack((average[144:],average[:144]))
+	if year == 2014:
+		avg = np.hstack((average[120:],average[:120]))
+	return avg
+
+def reshape(f, year):
+	data = genfromtxt(f, delimiter=',')[:, -1]
+	all = data[1:8737]
+	all = all.reshape((len(all))/(24*7), 24*7)
+	all_avg = np.mean(all, axis=0)
+	all_avg = rearrange(all_avg, year)
+	return all_avg
+
+
+def plot(files):
+	weekdaylst = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+	# colors = ['FIREBRICK','CRIMSON','DARKOLIVEGREEN','DARKGREEN','YELLOWGREEN']
+	colors = ['0.3','0.45','0.6','0.75','0.9']
+	fig2= plt.figure()
+	ax2 =fig2.add_axes([0.1,0.1,0.8,0.8],polar=True)
+	plt.subplots_adjust( left=0.05, bottom=0.05, right=0.95, top=0.95)
+	x = np.arange(-180.0,180.0,360/168.)
+	theta = (np.pi/180.)*x
+	offset =2.
+	
+	for count in range(5):
+		f = files[count]
+		year = int(f[-8:-4])
+		all = reshape(f, year)
+		ax2.plot(theta, all, color = colors[count], label = year,linewidth=2)
+		# ax2.plot(all)
+		ax2.set_ylabel('pm2.5')
+		ax2.set_xlim([0,24*7])
+		ax2.set_ylim([5,17])
+
+		major_ticks = np.arange(0, 7*24, 24)
+		minor_ticks = np.arange(0, 7*24, 1) 
+
+		ax2.tick_params(axis = 'both', which = 'major', labelsize = 12)
+		ax2.tick_params(axis = 'both', which = 'minor', labelsize = 12)
+
+		ax2.set_xticklabels([])
+
+		ax2.set_xticks(major_ticks)
+		ax2.set_xticks(minor_ticks, minor = True)
+		ax2.tick_params(which = 'major', direction = 'out')
+
+
+		ax2.grid(which = 'minor', alpha = 0.3)
+		ax2.grid(which = 'major')
+
+		ax2.set_title(year, fontsize='12' )
+	ax2.set_xticklabels(weekdaylst)
+
+	handles, labels = ax2.get_legend_handles_labels()
+	ax2.legend(handles, labels, loc=2)
+
+	plt.grid(which='major', alpha=0.5)
+	
+
+	plt.show()
+
+if __name__ == '__main__':
+	files = glob.glob('./PS19_2010_2014/MONITOR_'+'*.csv')
+	# print files
+	plot(files)
+	
+
